87de84c1ac67b6591a9f58ca7ee357d17028a5cf
akio0911 <akio0911@gmail.com>
2022-03-01 18:42:20 +0900

    feedback

--- KotaKadai11/PrefecturesViewController.swift
+++ KotaKadai11/PrefecturesViewController.swift
@@ -9,8 +9,10 @@ import UIKit
 
 class PrefecturesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    @IBOutlet weak var tableView: UITableView!
-    let prefectures = ["北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県", "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県", "静岡県", "愛知県", "三重県", "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県", "鳥取県", "島根県", "岡山県", "広島県", "山口県", "徳島県", "香川県", "愛媛県", "高知県", "福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"]
+    private(set) var selectedPrefectureName: String?
+
+    @IBOutlet private weak var tableView: UITableView!
+    private let prefectures = ["北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県", "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県", "静岡県", "愛知県", "三重県", "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県", "鳥取県", "島根県", "岡山県", "広島県", "山口県", "徳島県", "香川県", "愛媛県", "高知県", "福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"]
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         prefectures.count
@@ -21,6 +23,11 @@ class PrefecturesViewController: UIViewController, UITableViewDelegate, UITableV
         cell.textLabel?.text = prefectures[indexPath.row]
 
         return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedPrefectureName = prefectures[indexPath.row]
+        performSegue(withIdentifier: "exitSelected", sender: nil)
     }
 
 }
